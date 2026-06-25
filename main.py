67f9b13241aaa0f9ad5fe6fa8348dee8a7359a52
esat-ekici <esatekici@yahoo.com>
2026-06-25 08:28:27 +0300

    yahoo test

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+import yfinance as yf
 from telegram_sender import send_message
 
-send_message("✅ BIST BOT V2 ÇALIŞTI")
+symbol = "ASELS.IS"
+
+df = yf.download(
+    symbol,
+    period="5d",
+    auto_adjust=True,
+    progress=False
+)
+
+last_close = float(df["Close"].iloc[-1])
+
+message = (
+    f"📊 Veri Testi\n\n"
+    f"Hisse: {symbol}\n"
+    f"Son Fiyat: {last_close:.2f}"
+)
+
+send_message(message)
