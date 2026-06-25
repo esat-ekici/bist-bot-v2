c9da2697f92e966edf184c2459abe527f01602bd
esat-ekici <esatekici@yahoo.com>
2026-06-25 09:08:22 +0300

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -2,11 +2,15 @@ import yfinance as yf
 from indicators import calculate_indicators
 
 
+def ema(series, period):
+    return series.ewm(span=period, adjust=False).mean()
+
+
 def scan_symbol(symbol):
 
     df = yf.download(
         symbol,
-        period="2y",
+        period="3y",
         auto_adjust=True,
         progress=False
     )
@@ -29,21 +33,50 @@ def scan_symbol(symbol):
         / high52
     ) * 100
 
-    signal = (
-        close.iloc[-1] > sma50.iloc[-1]
-        and close.iloc[-1] > sma200.iloc[-1]
-        and 40 <= rsi.iloc[-1] <= 70
-        and volume_growth > 1.10
-    )
+    # ------------------
+    # HAFTALIK MACD
+    # ------------------
+
+    weekly_close = close.resample("W-FRI").last()
+
+    ema12 = ema(weekly_close, 12)
+    ema26 = ema(weekly_close, 26)
+
+    macd = ema12 - ema26
+    signal = ema(macd, 9)
+
+    macd_positive = macd.iloc[-1] > signal.iloc[-1]
+
+    # ------------------
+    # PUANLAMA
+    # ------------------
+
+    score = 0
+
+    if close.iloc[-1] > sma200.iloc[-1]:
+        score += 3
+
+    if close.iloc[-1] > sma50.iloc[-1]:
+        score += 2
+
+    if 45 <= rsi.iloc[-1] <= 70:
+        score += 1
+
+    if volume_growth > 1:
+        score += 2
+
+    if distance_to_high < 15:
+        score += 2
+
+    if macd_positive:
+        score += 3
 
     return {
         "symbol": symbol,
         "close": round(float(close.iloc[-1]), 2),
+        "score": score,
         "rsi": round(float(rsi.iloc[-1]), 1),
-        "above_sma50": close.iloc[-1] > sma50.iloc[-1],
-        "above_sma200": close.iloc[-1] > sma200.iloc[-1],
-        "volume_growth": round(float(volume_growth), 2),
-        "rel_volume": round(float(rel_volume), 2),
         "distance_to_high": round(float(distance_to_high), 1),
-        "signal": signal
+        "volume_growth": round(float(volume_growth), 2),
+        "macd_positive": macd_positive
     }
